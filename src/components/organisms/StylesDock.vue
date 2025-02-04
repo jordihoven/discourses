df72d1860aaffafa27fa1176e022c235d42e85ec
Jordi Hoven <jordi.hoven@precedence.nl>
2025-02-04 16:55:21 +0100

    cleaning up editorjs styles

---
+++ src/components/organisms/StylesDock.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="dock">
+    <div class="block">Heading</div>
+    <div class="block">Body</div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.dock {
+  border: var(--border);
+  border-radius: var(--radius);
+  background-color: var(--background);
+  padding: var(--xs-spacing);
+  width: fit-content;
+  display: flex;
+  gap: var(--xs-spacing);
+  position: fixed;
+  bottom: var(--m-spacing);
+  left: 50%;
+  transform: translate(-50%);
+  z-index: 69;
+}
+
+.block {
+  padding: var(--xs-spacing);
+  border: var(--border);
+  border-radius: var(--radius-sm);
+  background-color: var(--background2);
+  transition: var(--transition);
+}
+.block:hover {
+  cursor: pointer;
+}
+</style>
